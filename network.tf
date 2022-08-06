b5ec9f39e58c204d98c9b88c965ea47a4f9fe75f
Lakshmi Srinivas <srinivas486@gmail.com>
2022-08-06 03:58:44 +0000

    Network for Gaurav

--- network.tf
+++ network.tf
@@ -1,7 +1,11 @@
+resource "azurerm_resource_group" "example" {
+  name     = "my-resources"
+  location = "West Europe"
+}
 module "network" {
   source  = "app.terraform.io/skillfield/network/azurerm"
   version = "3.5.0"
   # insert required variables here
-  resource_group_name = "${var.prefix}-gaurav-workshop"
+  resource_group_name = "azurerm_resource_group.example.name"
 }
 
